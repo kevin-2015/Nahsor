231bba469736624a6d4a7d3fbe52e5f3b28d1654
e <stormcaohp@gmail.com>
2018-05-14 11:21:27 +0700

    将所有接口写入一个变量

---
+++ app/static/js/interface.js
@@ -0,0 +1,6 @@
+var interfaceUrl = {
+    "testcase"  : "http://118.24.105.78:8808/querytcass",
+    "project"   : "http://118.24.105.78:8808/queryproject",
+    "product"   : "http://118.24.105.78:8808/queryproduct",
+    "modules"   : "http://118.24.105.78:8808/querymodule"    
+};
