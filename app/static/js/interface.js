e19e1559914f28470c9e91ce1ff2de5fceff0fd4
e <stormcaohp@gmail.com>
2018-05-15 14:58:12 +0700

    完成产品管理中的增删改功能

--- app/static/js/interface.js
+++ app/static/js/interface.js
@@ -1,6 +1,9 @@
 var interfaceUrl = {
-    "testcase"  : "http://118.24.105.78:8808/querytcass",
-    "project"   : "http://118.24.105.78:8808/queryproject",
-    "product"   : "http://118.24.105.78:8808/queryproduct",
-    "modules"   : "http://118.24.105.78:8808/querymodule"    
+    "testcase"      : "http://118.24.105.78:8808/querytcass",
+    "project"       : "http://118.24.105.78:8808/queryproject",
+    "product"       : "http://118.24.105.78:8808/queryproduct",
+    "modules"       : "http://118.24.105.78:8808/querymodule",
+    "addproduct"    : "http://118.24.105.78:8808/addproduct",
+    "deleteproduct" : "http://118.24.105.78:8808/deleteproduct",
+    "updataproduct" : "http://118.24.105.78:8808/updataproduct"
 };
