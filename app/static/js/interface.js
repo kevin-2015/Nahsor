43da064e8b4ec1fe5cef11868191b06bf6e62f77
snake <snake_goodluck@hotmail.com>
2018-05-16 11:11:04 +0800

    fix

--- app/static/js/interface.js
+++ app/static/js/interface.js
@@ -1,19 +1,19 @@
-// var interfaceUrl = {
-//     "testcase"      : "http://118.24.105.78:8808/querytcass",
-//     "project"       : "http://118.24.105.78:8808/queryproject",
-//     "product"       : "http://118.24.105.78:8808/queryproduct",
-//     "modules"       : "http://118.24.105.78:8808/querymodule",
-//     "addproduct"    : "http://118.24.105.78:8808/addproduct",
-//     "deleteproduct" : "http://118.24.105.78:8808/deleteproduct",
-//     "updataproduct" : "http://118.24.105.78:8808/updataproduct"
-// };
-
 var interfaceUrl = {
-    "testcase"      : "http://localhost:8808/querytcass",
-    "project"       : "http://localhost:8808/queryproject",
-    "product"       : "http://localhost:8808/queryproduct",
-    "modules"       : "http://localhost:8808/querymodule",
-    "addproduct"    : "http://localhost:8808/addproduct",
-    "deleteproduct" : "http://localhost:8808/deleteproduct",
-    "updataproduct" : "http://localhost:8808/updataproduct"
+    "testcase"      : "http://118.24.105.78:8808/querytcass",
+    "project"       : "http://118.24.105.78:8808/queryproject",
+    "product"       : "http://118.24.105.78:8808/queryproduct",
+    "modules"       : "http://118.24.105.78:8808/querymodule",
+    "addproduct"    : "http://118.24.105.78:8808/addproduct",
+    "deleteproduct" : "http://118.24.105.78:8808/deleteproduct",
+    "updataproduct" : "http://118.24.105.78:8808/updataproduct"
 };
+
+// var interfaceUrl = {
+//     "testcase"      : "http://localhost:8808/querytcass",
+//     "project"       : "http://localhost:8808/queryproject",
+//     "product"       : "http://localhost:8808/queryproduct",
+//     "modules"       : "http://localhost:8808/querymodule",
+//     "addproduct"    : "http://localhost:8808/addproduct",
+//     "deleteproduct" : "http://localhost:8808/deleteproduct",
+//     "updataproduct" : "http://localhost:8808/updataproduct"
+// };
