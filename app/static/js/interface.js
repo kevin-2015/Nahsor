02b9e6ba1c60a8a8f665480f42ff7bdb18b7c5e4
e <stormcaohp@gmail.com>
2018-05-19 10:58:27 +0700

    项目管理模块

--- app/static/js/interface.js
+++ app/static/js/interface.js
@@ -1,19 +1,18 @@
 var interfaceUrl = {
+    //产品管理
     "testcase"      : "http://118.24.105.78:8808/querytcass",
     "project"       : "http://118.24.105.78:8808/queryproject",
     "product"       : "http://118.24.105.78:8808/queryproduct",
     "modules"       : "http://118.24.105.78:8808/querymodule",
     "addproduct"    : "http://118.24.105.78:8808/addproduct",
     "deleteproduct" : "http://118.24.105.78:8808/deleteproduct",
-    "updataproduct" : "http://118.24.105.78:8808/updataproduct"
+    "updataproduct" : "http://118.24.105.78:8808/updataproduct",
+    //项目管理
+    "addproject"    : "http://118.24.105.78:8808/addproject",   //新增项目
+    "getproduct"    : "http://118.24.105.78:8808/getproduct",   //获取所有产品
+    "queryproject"  : "http://118.24.105.78:8808/queryproject", //咨询项目列表
+    "deleteproject" : "http://118.24.105.78:8808/deleteproject",//删除项目
+    "readproject"   : "http://118.24.105.78:8808/readproject",  //读取项目信息
+    "updataproject" : "http://118.24.105.78:8808/updataproject",//更新产品信息
+    "runproject"    : "http://118.24.105.78:8808/runproject"    //按项目执行所有用例
 };
-
-// var interfaceUrl = {
-//     "testcase"      : "http://localhost:8808/querytcass",
-//     "project"       : "http://localhost:8808/queryproject",
-//     "product"       : "http://localhost:8808/queryproduct",
-//     "modules"       : "http://localhost:8808/querymodule",
-//     "addproduct"    : "http://localhost:8808/addproduct",
-//     "deleteproduct" : "http://localhost:8808/deleteproduct",
-//     "updataproduct" : "http://localhost:8808/updataproduct"
-// };
