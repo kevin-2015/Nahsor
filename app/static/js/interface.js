6fa3b0c154f43005b37d5178580eb885a83c17a0
Bai <fenyukuang@gmail.com>
2018-05-19 14:45:37 +0800

    add api

--- app/static/js/interface.js
+++ app/static/js/interface.js
@@ -1,12 +1,13 @@
 var interfaceUrl = {
     //产品管理
-    "testcase"      : "http://118.24.105.78:8808/querytcass",
-    "project"       : "http://118.24.105.78:8808/queryproject",
-    "product"       : "http://118.24.105.78:8808/queryproduct",
-    "modules"       : "http://118.24.105.78:8808/querymodule",
-    "addproduct"    : "http://118.24.105.78:8808/addproduct",
-    "deleteproduct" : "http://118.24.105.78:8808/deleteproduct",
-    "updataproduct" : "http://118.24.105.78:8808/updataproduct",
+    "testcase"      : "http://118.24.105.78:8808/querytcass",  //查询用例列表
+    "project"       : "http://118.24.105.78:8808/queryproject",  //查询项目列表
+    "product"       : "http://118.24.105.78:8808/queryproduct",  //查询产品列表
+    "modules"       : "http://118.24.105.78:8808/querymodule",  //查询模块列表
+    "addproduct"    : "http://118.24.105.78:8808/addproduct",  //新增产品
+    "deleteproduct" : "http://118.24.105.78:8808/deleteproduct",  //删除产品
+    "updataproduct" : "http://118.24.105.78:8808/updataproduct",  //更新产品信息
+    "runproduct" : "http://118.24.105.78:8808/runproduct",  //更新产品信息
     //项目管理
     "addproject"    : "http://118.24.105.78:8808/addproject",   //新增项目
     "getproduct"    : "http://118.24.105.78:8808/getproduct",   //获取所有产品
@@ -14,5 +15,22 @@ var interfaceUrl = {
     "deleteproject" : "http://118.24.105.78:8808/deleteproject",//删除项目
     "readproject"   : "http://118.24.105.78:8808/readproject",  //读取项目信息
     "updataproject" : "http://118.24.105.78:8808/updataproject",//更新产品信息
-    "runproject"    : "http://118.24.105.78:8808/runproject"    //按项目执行所有用例
+    "runproject"    : "http://118.24.105.78:8808/runproject",    //按项目执行所有用例
+    //模块管理
+    "addmodule"    : "http://118.24.105.78:8808/addmodule",   //新增模块
+    "getproject"    : "http://118.24.105.78:8808/getproject",   //获取上级项目和产品
+    "querymodule"  : "http://118.24.105.78:8808/querymodule", //查询模块列表
+    "deletemodule" : "http://118.24.105.78:8808/deletemodule",//删除模块
+    "readmodule"   : "http://118.24.105.78:8808/readmodule",  //读取模块信息
+    "updatamodule" : "http://118.24.105.78:8808/updatamodule",//更新模块信息
+    "runmodule"    : "http://118.24.105.78:8808/runmodule",    //按模块执行所有用例
+    //用例管理
+    "addtcass"    : "http://118.24.105.78:8808/addtcass",   //新增用例
+    "getmodules"    : "http://118.24.105.78:8808/getmodules",   //获取所有用例
+    "querytcass"  : "http://118.24.105.78:8808/querytcass", //查询用例列表
+    "deletecass" : "http://118.24.105.78:8808/deletecass",//删除用例
+    "readcass"   : "http://118.24.105.78:8808/readcass",  //读取用例信息
+    "updatacass" : "http://118.24.105.78:8808/updatacass",//更新用例信息
+    "runtests"    : "http://118.24.105.78:8808/runtests",    //按用例执行所有用例
+    "getcassres"    : "http://118.24.105.78:8808/getcassres"    //查询用例的执行结果
 };
