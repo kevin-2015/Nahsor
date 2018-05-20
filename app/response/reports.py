1fecb1afa8203949b66ee7177ede18f147fcf655
testjie <snake_goodluck@hotmail.com>
2018-05-20 20:22:22 +0800

    update reports

--- app/response/reports.py
+++ app/response/reports.py
@@ -9,27 +9,59 @@ from app.utils.log import Logger
 Logger = Logger()
 
 
-@bp.route("/getreports", methods=['post'])
-def getreports():
+def _get_test_results(**kwargs):
+    """
+    抽象查询结果的公共方法
+    :params kwargs: 参数可以传任意值
+        {} / {"version": 14} / {"version": 14, "productid": 1}
+        {"version": 14, "productid": 1, "projectid": 1} / {"version": 14, "productid": 1, "projectid": 1, "moduleid": 6}
+    :return:
+    """
+    version = kwargs.get("version")
+    productid = kwargs.get("productid")
+    projectid = kwargs.get("projectid")
+    moduleid = kwargs.get("moduleid")
+    # 构造sql
+    product_sql, project_sql, module_sql = ["", ""], ["", ""], ["", ""]
+    if productid is not None and productid != "":
+        product_sql.clear()
+        product_sql.append(" ,t_product AS b")
+        product_sql.append(" AND b.id=%d" % productid)
+    if projectid is not None and projectid != "":
+        project_sql.clear()
+        project_sql.append(" ,t_project AS c")
+        project_sql.append(" AND c.id=%d" % projectid)
+    if moduleid is not None and moduleid != "":
+        module_sql.clear()
+        module_sql.append(" ,t_modules AS d")
+        module_sql.append(" AND d.id=%d  AND e.moduleid = d.id" % moduleid)
+    if version is not None and version != "":
+        version_sql = "a.version = %d" % version
+    else:
+        version_sql = " version in (select max(version) as version from t_reports)"
+
+    success_sql = "SELECT a.* FROM t_reports AS a %s %s  %s ,t_testcass as e WHERE %s AND a.`status`='成功' %s %s %s" \
+                  " AND a.cassid = e.id" % (product_sql[0], project_sql[0], module_sql[0], version_sql, product_sql[1], project_sql[1], module_sql[1])
+
+    failed_sql = "SELECT a.* FROM t_reports AS a %s %s  %s ,t_testcass as e WHERE %s AND a.`status`!='成功' %s %s %s" \
+                  " AND a.cassid = e.id" % (product_sql[0], project_sql[0], module_sql[0], version_sql, product_sql[1], project_sql[1], module_sql[1])
+
+    all_sql = "SELECT a.* FROM t_reports AS a %s %s  %s ,t_testcass as e WHERE %s %s %s %s" \
+                  " AND a.cassid = e.id" % (product_sql[0], project_sql[0], module_sql[0], version_sql, product_sql[1], project_sql[1], module_sql[1])
+
+    return dbfucs.query(success_sql), dbfucs.query(failed_sql), dbfucs.query(all_sql)
+
+
+@bp.route("/conditionsqueryreports", methods=['post'])
+def conditionsqueryreports():
     """
     获取根据版本获取用例执行结果
     {"version":""}：默认最大
     {"version":5}：版本为5的报告
     :return:
     """
-    dictdata = request.get_json()
-    if dictdata is None or dictdata.get("version") is None or dictdata.get("version") == "":
-        sql = "SELECT * FROM t_reports \
-                WHERE version = (SELECT max(version) FROM t_reports)"
-    else:
-        sql = "SELECT * FROM t_reports \
-                WHERE version = %d" % dictdata.get("version")
-
-    res = dbfucs.query(sql)
-    response = {}
-    response["code"] = 200
-    response["data"] = res
-    response["msg"] = "查询成功！！！"
+    success_results, failed_results, all_results = _get_test_results(**request.get_json())
+    response = {"code": 200, "data": all_results, "msg": "查询成功！！！"}
     return jsonify(response)
 
 
@@ -50,23 +82,16 @@ def getversions():
     return jsonify(response)
 
 
-"""
-    1. 条件查询
-    1. 版本作为基础
-    2. 可以进行产品、项目、模块来进行筛选
-    3. 涉及到饼图、柱状图、记录
-
-"""
-
-
-def _get_test_results_by_conditions(**kwargs):
-    """1. 根据条件获得用例执行结果
-        :arg   参数可以传任意值
-        {} / {"version": 14} / {"version": 14, "productid": 1}
-        {"version": 14, "productid": 1, "projectid": 1} / {"version": 14, "productid": 1, "projectid": 1, "moduleid": 6}
-    : return :
+@bp.route("/conditionquerytestress", methods=["post"])
+def conditionquerytestres():
     """
+    条件查询测试结果
+    :param   参数可以传任意值
+    {} / {"version": 14} / {"version": 14, "productid": 1}
+    {"version": 14, "productid": 1, "projectid": 1} / {"version": 14, "productid": 1, "projectid": 1, "moduleid": 6}
+    :return:
     """
+    """ 返回值:
           "testreports": {
             "failed": {
                 "count": 0,
@@ -100,49 +125,25 @@ def _get_test_results_by_conditions(**kwargs):
         }
 
     """
-
-    version = kwargs.get("version")
-    productid = kwargs.get("productid")
-    projectid = kwargs.get("projectid")
-    moduleid = kwargs.get("moduleid")
-    # 构造sql
-    product_sql, project_sql, module_sql = ["", ""], ["", ""], ["", ""]
-    if productid is not None and productid != "":
-        product_sql.clear()
-        product_sql.append(" ,t_product AS b")
-        product_sql.append(" AND b.id=%d" % productid)
-    if projectid is not None and projectid != "":
-        project_sql.clear()
-        project_sql.append(" ,t_project AS c")
-        project_sql.append(" AND c.id=%d" % projectid)
-    if moduleid is not None and moduleid != "":
-        module_sql.clear()
-        module_sql.append(" ,t_modules AS d")
-        module_sql.append(" AND d.id=%d  AND e.moduleid = d.id" % moduleid)
-    if version is not None and version != "":
-        version_sql = "a.version = %d" % version
-    else:
-        version_sql = " version in (select max(version) as version from t_reports)"
-
-    success_sql = "SELECT a.* FROM t_reports AS a %s %s  %s ,t_testcass as e WHERE %s AND a.`status`='成功' %s %s %s" \
-                  " AND a.cassid = e.id" % (product_sql[0], project_sql[0], module_sql[0], version_sql, product_sql[1], project_sql[1], module_sql[1])
-
-    failed_sql = "SELECT a.* FROM t_reports AS a %s %s  %s ,t_testcass as e WHERE %s AND a.`status`!='成功' %s %s %s" \
-                  " AND a.cassid = e.id" % (product_sql[0], project_sql[0], module_sql[0], version_sql, product_sql[1], project_sql[1], module_sql[1])
-
-    success_results = dbfucs.query(success_sql)
-    failed_results = dbfucs.query(failed_sql)
-
-    last_testreports = {}
+    res, last_testreports = {}, {}
+    success_results, failed_results, all_results = _get_test_results(**request.get_json())
     last_testreports["success"] = {"data": success_results, "count": len(success_results)}
     last_testreports["failed"] = {"data": failed_results, "count": len(failed_results)}
 
-    return last_testreports
+    response = {"code": 200, "data": last_testreports, "msg": "查询成功！！！"}
+    return jsonify(response)
 
 
-# 该做这里的条件统计了
-def _get_test_result_runtime_by_conditions(**kwargs):
-    """2. 上次用例执行时间分段统计图"""
+
+@bp.route("/conditionqueryruntimes", methods=["post"])
+def conditionqueryruntimes():
+    """
+    条件查询用例执行时间分段统计图
+    :param ： 可以传以下任意参数
+    {} / {"version": 14} / {"version": 14, "productid": 1}
+    {"version": 14, "productid": 1, "projectid": 1} / {"version": 14, "productid": 1, "projectid": 1, "moduleid": 6}
+    : return :
+    """
     """
         时间分段统计：
         极快: t<1s
@@ -150,40 +151,44 @@ def _get_test_result_runtime_by_conditions(**kwargs):
         慢: 3s < t < 5s
         超级慢: t>5s
 
-        "runtimecount": {
+     {
+        "code": 200,
+        "data": {
             "fast": {
                 "count": 0,
                 "times": []
             },
             "faster": {
-                "count": 2,
-                "times": [
-                    0.011257,
-                    0.012227
-                ]
+                "count": 0,
+                "times": []
             },
             "slow": {
                 "count": 0,
                 "times": []
             },
             "slowly": {
-                "count": 0,
-                "times": []
+                "count": 2,
+                "times": [
+                    100,
+                    100
+                ]
             }
         },
+        "msg": "查询成功!"
+}
     """
-    faster, fast, slow, slowly = {}, {}, {}, {}
+
+    # 各个参数
+    res, last_testreports, faster, fast, slow, slowly = {}, {}, {}, {}, {}, {}
     faster["count"], fast["count"], slow["count"], slowly["count"] = 0, 0, 0, 0
     faster["times"], fast["times"], slow["times"], slowly["times"], = [], [], [], []
-    success_sql = "select * from t_reports where version in" \
-                  " (select max(version) as version from t_reports) and status=0"
-    failed_sql = "select * from t_reports where version in" \
-                 " (select max(version) as version from t_reports) and status!=0"
-    success_results = dbfucs.query(success_sql)
-    failed_results = dbfucs.query(failed_sql)
+    success_results, failed_results, all_results = _get_test_results(**request.get_json())
     for result in (success_results, failed_results):
         for r in result:
-            runtime = float(r.get("runtime"))
+            try:
+                runtime = float(r.get("runtime"))
+            except:
+                runtime = 100.0
             if runtime < 1.0:
                 faster["count"] += 1
                 faster["times"].append(runtime)
@@ -197,13 +202,8 @@ def _get_test_result_runtime_by_conditions(**kwargs):
                 slowly["count"] += 1
                 slowly["times"].append(runtime)
 
-    return {"faster": faster, "fast": fast, "slow": slow, "slowly": slowly}
-
-
-@bp.route("/conditionquerytestress", methods=["post"])
-def conditionquerytestres():
-    res = {}
+    # 返回值
     res["code"] = 200
-    res["data"] = _get_test_results_by_conditions(**request.get_json())
+    res["data"] = {"faster": faster, "fast": fast, "slow": slow, "slowly": slowly}
     res["msg"] = "查询成功!"
     return jsonify(res)
